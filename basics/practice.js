4f9a4222c43852aeb8c5bd950d04c75d92a0fa23
Arpitpratap02 <arpitprataparpit@gmail.com>
2024-07-16 22:07:20 +0530

    start basics_2

--- basics/practice.js
+++ basics/practice.js
@@ -22,7 +22,16 @@ const score = 400;
 
 // console.log(score.toString().length);
 // console.log(score.toFixed(1));
-console.log(Math.abs(-4));
-console.log(Math.round(4.4));
-console.log(Math.ceil(4.4));
-console.log(`floor function ${Math.floor(4.4)}`);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.4));
+// console.log(`floor function ${Math.floor(4.4)}`);
+
+let myDate = new Date();
+
+console.log(myDate.getDate());
+console.log(myDate.toLocaleString());
+console.log(myDate.toJSON());
+
+let myCreatedDate = new Date(2023, 1, 22)
+console.log(myCreatedDate.toString());
---
+++ basics_2/arrays.js
@@ -0,0 +1,2 @@
+const arr = ["arpit",0,23,56];
+console.log(arr[0]);
